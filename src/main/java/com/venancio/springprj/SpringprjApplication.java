94370a7f0723cd9e08804484f3d803fc43c0f38b
Rameg <italimanooo@gmail.com>
2019-07-24 10:27:34 -0300

    Operacao de Instaciacao na Base

--- src/main/java/com/venancio/springprj/SpringprjApplication.java
+++ src/main/java/com/venancio/springprj/SpringprjApplication.java
@@ -1,13 +1,31 @@
 package com.venancio.springprj;
 
+import com.venancio.springprj.domain.Categoria;
+import com.venancio.springprj.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringprjApplication {
+public class SpringprjApplication implements CommandLineRunner  {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringprjApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
 }
