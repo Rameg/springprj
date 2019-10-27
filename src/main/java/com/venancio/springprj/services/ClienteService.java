c3964107de2818255c64167028a614344d810866
Adeeb Venancio <italimanooo@gmail.com>
2019-10-27 10:55:12 -0700

    ClienteResource e ClienteService

---
+++ src/main/java/com/venancio/springprj/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.venancio.springprj.services;
+
+import com.venancio.springprj.domain.Cliente;
+import com.venancio.springprj.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente find(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}
